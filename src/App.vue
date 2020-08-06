951529d60bd3958ae066eb7bd7ad8d279b19d757
Russ <851911336@qq.com>
2020-08-06 23:50:01 +0800

    添加Money.vue的html  添加全局样式全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,20 @@
 <template>
-  <div >
-   <router-view />
+  <div>
+    <router-view/>
 
   </div>
 </template>
 
 <style lang="scss">
-  * {
-    box-sizing: border-box;
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
+  body{
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: $font-hei;
+    color: #333;
+    line-height: 1.5;
   }
-  *:before,
-  *:after {
-    box-sizing: border-box;
-  }
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  ul,
-  ol {
-    list-style: none;
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  img {
-    max-width: 100%;
-    max-height: 100%;
-  }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    labels
+    <p>labels</p>
   </Layout>
 
 </template>
