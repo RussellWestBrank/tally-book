8de8ade3e1e5f8c805d4c744936e9a76423a1635
Russ <851911336@qq.com>
2020-08-10 14:59:08 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <label class="notes">
-      <span class="name">{{fieldName}}</span>
-      <input type="text" v-model="value"
+    <label class="formItem">
+      <span class="name">{{this.fieldName}}</span>
+      <input type="text"
+             v-model="value"
              :placeholder="this.placeholder">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+  import Vue from 'vue';
   import {Component, Prop, Watch} from 'vue-property-decorator';
-
   @Component
-  export default class Notes extends Vue {
-    @Prop({ required: true }) readonly fieldName!: string
-    @Prop() readonly placeholder!: string
+  export default class FormItem extends Vue {
     value = '';
+    @Prop({required: true}) fieldName!: string;
+    @Prop() placeholder?: string;
     @Watch('value')
     onValueChanged(value: string) {
       this.$emit('update:value', value);
@@ -25,9 +25,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
-    background: #f5f5f5;
     padding-left: 16px;
     display: flex;
     align-items: center;
@@ -35,7 +34,7 @@
       padding-right: 16px;
     }
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
